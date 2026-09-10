927bfb59e1012d1df8ac7e2dc0f6c2d90e5b5c33
ALEX GABRIEL ALCOCER RODRIGUEZ <139175706+alex123566545@users.noreply.github.com>
2026-09-10 07:55:18 -0500

    .

--- pages/verificacion.py
+++ pages/verificacion.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date, datetime, timedelta, timezone
 
-from src.config.database import get_connection
+from utils.database import get_connection
 
 st.set_page_config(
     page_title="SIPREM-BOVINO | Verificación",
